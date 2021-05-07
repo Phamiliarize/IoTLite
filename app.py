532de14c9ee1b4e8bfd2fb5645fe65c072aec8ff
Jesse Pham <10214230+Phamiliarize@users.noreply.github.com>
2021-05-07 15:56:56 +0900

    Basic Command/Connectivity Complete

--- app.py
+++ app.py
@@ -1,21 +1,53 @@
-from chalice import Chalice
-from chalice import BadRequestError
+import boto3
+import json
+
+from chalice import Chalice, BadRequestError, ChaliceViewError, NotFoundError
 
 app = Chalice(app_name='IoTLite')
+awsIotData = boto3.client('iot-data')
+awsIot = boto3.client('iot')
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
 
-@app.route('/thing/{thingId}', methods=['GET'])
-def one_thing(thingId):
-    return {'thing': thingId }
+@app.route('/thing', methods=['GET'])
+def list_thing():
+    try:
+        response = awsIot.search_index(
+            queryString='thingName:*'
+        )
+        return response["things"]
+    except (Exception, KeyError) as e:
+        print(e)
+        raise ChaliceViewError("A server error has occurred.")
 
-@app.route('/thing/{thingId}/command/{command}', methods=['POST'])
-def one_thing_command(thingId, command):
+@app.route('/thing/{name}', methods=['GET'])
+def one_thing(name):
+    try:
+        response = awsIot.search_index(
+            queryString='thingName:{}'.format(name)
+        )
+        return response["things"][0]
+    except (IndexError, awsIot.exceptions.ResourceNotFoundException):
+        raise NotFoundError("The requested thing could not be found.")
+    except Exception as e:
+        print(e)
+        raise ChaliceViewError("A server error has occurred.")
+
+@app.route('/thing/{name}/command/{command}', methods=['POST'])
+def one_thing_command(name, command):
     payload = command_switch(command)
-    if not payload:
-        return { "error":"unsupported command"}
+    try: 
+        response = awsIotData.publish(
+            topic='{}/command'.format(name),
+            qos=1,
+            payload=json.dumps(payload).encode('utf-8')
+        )
+    except Exception as e:
+        print(e)
+        raise ChaliceViewError("A server error has occurred.")
+
     return payload
 
 def command_switch(argument):
